9e6378b80aa8196639f90c12e9e6887489c6ea86
felippehsk <feh_karp@hotmail.com>
2019-04-04 10:44:44 -0500

    Update

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -3,12 +3,3 @@ let OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapn
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap)
-// L.tileLayer.wms('http://mesonet.agron.iastate.edu/cgi-bin/wms/nexrad/n0r.cgi', {
-//   layers: 'nexrad-n0r-900913',
-//   format: 'image/png',
-//   transparent: true,
-//   attribution: 'NOAA, Iowa State University'
-// }).addTo(myMap)
-L.esri.dynamicMapLayer({
-  url: 'https://nowcoast.noaa.gov/arcgis/rest/services/nowcoast/radar_meteo_imagery_nexrad_time/MapServer'
-}).addTo(myMap)
