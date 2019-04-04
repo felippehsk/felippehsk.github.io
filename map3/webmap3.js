4efb553b2fd240a808f5e63e87453797dad30cf9
felippehsk <feh_karp@hotmail.com>
2019-04-04 11:04:59 -0500

    Update

--- map3/webmap3.js
+++ map3/webmap3.js
@@ -1,5 +1,11 @@
 let  myMap = L.map('map3').setView([32.18, -99.14], 4)
-let OpenStreetMap_BlackAndWhite = L.tileLayer('https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+let OpenStreetMap_BlackAndWhite = 'https://tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png'
+L.tileLayer(OpenStreetMap_BlackAndWhite, {
 	maxZoom: 18,
 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap)
+
+let farmData = 'https://github.com/felippehsk/felippehsk.github.io/tree/master/Data/Data_3.geojson'
+jQuery.getJSON(farmData, function(data){
+	L.geoJSON(data).addTo(myMap)
+})
